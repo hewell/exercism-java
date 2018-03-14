a1482d13679d5ce8ac4a3c75e9c76fa97f727aa9
hewell <hewell@gmail.com>
2018-03-15 00:06:40 +0800

    #6 pangram using stream

--- pangram/src/main/java/PangramChecker.java
+++ pangram/src/main/java/PangramChecker.java
@@ -1,21 +1,11 @@
+import java.util.*;
+import java.util.stream.*;
+
 public class PangramChecker {
 
     public boolean isPangram(String input) {
-        String letters = "abcdefghijklmnopqrstuvwxyz";
-        boolean found = true;
-
-        if (input == null || input.length() == 0) {
-            return false;
-        }
-
         String lowerCaseInput = input.toLowerCase();
-        // check if every char appears in input
-        for (char c : letters.toCharArray()) {
-            if (lowerCaseInput.indexOf(c) == -1) {
-                found = false;
-            }
-        }
 
-        return found;
+        return IntStream.rangeClosed('a', 'z').mapToObj(i -> (char) i).allMatch(c -> lowerCaseInput.indexOf(c) > -1);
     }
 }
